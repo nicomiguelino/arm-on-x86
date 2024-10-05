277f339bb526a137bcd0465333b22fb37738261c
nicomiguelino <nicomiguelino2014@gmail.com>
2024-10-05 15:38:35 -0700

    chore: update pipeline for building the hello example

--- scripts/build_examples.sh
+++ scripts/build_examples.sh
@@ -3,20 +3,23 @@
 set -euo pipefail
 
 CORE_COUNT="$(nproc)"
+RELEASE_DIR='/build/release'
 
 function build_hello() {
     local EXAMPLES_DIR='/src/examples'
+    local EXAMPLE_RELEASE_DIR="${RELEASE_DIR}/hello"
+
+    mkdir -p ${EXAMPLE_RELEASE_DIR}
 
     cd ${EXAMPLES_DIR}/hello
     mkdir -p build && cd build
     cmake ..
     cmake --build . --parallel ${CORE_COUNT}
 
-    cp hello ${RELEASE_DIR}/hello-armv7
+    cp hello ${EXAMPLE_RELEASE_DIR}/hello-armv7
 }
 
 function main() {
-    local RELEASE_DIR='/build/release'
     mkdir -p ${RELEASE_DIR}
 
     build_hello
