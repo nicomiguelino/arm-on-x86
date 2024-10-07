d7c92c564e5752ddad87dfedbcf4b664ea2cdd27
nicomiguelino <nicomiguelino2014@gmail.com>
2024-10-07 16:02:55 -0700

    chore: enable debug logging; switch to C++17

--- examples/pingpong/src/pong.cpp
+++ examples/pingpong/src/pong.cpp
@@ -8,6 +8,10 @@
 #include <QDBusConnection>
 #include <QDBusError>
 
+#include <iostream>
+#include <fstream>
+#include <QDateTime>
+
 class Pong : public QObject
 {
     Q_OBJECT
@@ -26,6 +30,23 @@ int main(int argc, char **argv)
 {
     QCoreApplication app(argc, argv);
 
+
+    qInstallMessageHandler([](QtMsgType type, const QMessageLogContext &context, const QString &msg) {
+        std::ofstream logFile("/tmp/main.log", std::ios_base::app);
+        QString finalMessage = QString("[%1] %2\n").arg(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss")).arg(msg);
+
+        // Output 1: To console
+        std::cerr << finalMessage.toStdString();
+
+        // Output 2: To log file
+        if (logFile.is_open()) {
+            logFile << finalMessage.toStdString();
+            logFile.close();
+        } else {
+            std::cerr << "Failed to open log file." << std::endl;
+        }
+    });
+
     auto connection = QDBusConnection::sessionBus();
 
     if (!connection.isConnected()) {
@@ -43,8 +64,9 @@ int main(int argc, char **argv)
     Pong pong;
     connection.registerObject("/", &pong, QDBusConnection::ExportAllSlots);
 
-    app.exec();
-    return 0;
+    qDebug() << "PONG IS READY";
+
+    return app.exec();
 }
 
 #include "pong.moc"
