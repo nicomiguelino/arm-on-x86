378c8bcfa8a2587994dae9e2479cb44022155ea5
nicomiguelino <nicomiguelino2014@gmail.com>
2024-11-27 18:38:21 -0800

    fix: set up test containers to run `nicomiguelino/build-qt6-on-pi` binaries

--- pi/scripts/start_viewer.sh
+++ pi/scripts/start_viewer.sh
@@ -2,16 +2,7 @@
 
 set -euo pipefail
 
-function start_x_server() {
-    # Start X server in the background without cursor without sleep
-    X -nocursor -noreset :0 &
-}
-
 function main() {
-    start_x_server
-    sleep 2
-    xset s off -dpms
-
     sleep infinity
 }
 
